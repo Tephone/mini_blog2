98e98d71c11fa4861ede9e38fde1f2d8de80ff98
naoko kasahara <kassa0918@gmail.com>
2021-07-03 22:14:07 +0900

    未ログインユーザーアクセス制限

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, if: :user_auth?
+
   protected
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :blog_url])
+  end
+
+  def user_auth?
+    unless controller_name == 'blogs' && action_name == 'index'
+      true
+    end
   end
 end
