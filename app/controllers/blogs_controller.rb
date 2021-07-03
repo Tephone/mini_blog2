110b8efca3be3b3c8f84813b2100eb03050d6b89
naoko kasahara <kassa0918@gmail.com>
2021-07-03 15:39:08 +0900

    blog_index_new_create機能追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,18 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
   end
 
   def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(params.require(:blog).permit(:content))
+    if @blog.save
+      redirect_to blogs_path, notice: 'mini_blogを作成しました'
+    else
+      render :new
+    end
   end
 end
