7ab5b844f0f10a020d4ac893d141c6ed6fbf9149
naoko kasahara <kassa0918@gmail.com>
2021-07-03 21:57:19 +0900

    validation, dependent: :destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :blogs
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :blogs, dependent: :destroy
+  validates :profile, length: {maximum: 200}
+  validates :name, presence: true,
+                    length: {in: 1..20},
+                    format: {with: /\A[a-zA-Z]+\z/}
 end
