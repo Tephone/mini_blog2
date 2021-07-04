795964dfc52f966a07a62053fda2c800a3323975
naoko kasahara <kassa0918@gmail.com>
2021-07-04 14:10:51 +0900

    user編集機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :blog_url])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile, :blog_url])
   end
 
   def user_auth?
